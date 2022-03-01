247e87bd465e9eec0270c25966b5a060f665e223
HonggangBu <honggang.bu@ndsu.edu>
2022-02-28 20:35:04 -0600

    Initial commit

---
+++ NitrogenXamarin2/CommonData/SunflowerBaseTables.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NitrogenXamarin2.CommonData
+{
+    public static class SunflowerBaseTables
+    {
+    }
+}
---
+++ NitrogenXamarin2/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NitrogenXamarin2.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync("https://www.tutorialspoint.com/unix/index.htm", BrowserLaunchMode.SystemPreferred);
+        }
+    }
+}
